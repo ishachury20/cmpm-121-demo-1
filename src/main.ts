c662d68e9e44e8a49a5d95b305d3e9c198560d73
ishachury20 <35016233+ishachury20@users.noreply.github.com>
2024-10-05 22:50:31 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//let clicks = 0; 
+const button = document.createElement("button"); 
+button.innerHTML = "Cookie! 😄"; 
+
+
+app.append(button); 
