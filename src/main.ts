677da3f1835e3e2aab50c02fc9b1e61ac4cfc09b
ishachury20 <35016233+ishachury20@users.noreply.github.com>
2024-10-05 23:14:43 -0700

    step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,19 @@ app.append(header);
 let clicks = 0;
 const button = document.createElement("button");
 button.innerHTML = "Cookie! 😄";
-button.onclick = () => {
+
+//clicks counter taken from lecture video
+//watched youtube video to understand how to use setInterval
+//https://www.youtube.com/watch?v=JRevaOwNKTI
+
+setInterval(() => { 
     clicks++; 
-    button.innerHTML = `Cookie 😄 (${clicks})`; 
-}
+    button.innerHTML = `Cookie 😄 (${clicks})`;
+    button.onclick = () => {
+        clicks = clicks + 1; 
+        button.innerHTML = `Cookie 😄 (${clicks})`;
+        console.log(clicks)
+    } 
+}, 1000);
+
 app.append(button);
